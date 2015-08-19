336f9835670efd0f18c49e98c3d65f2fe9c58878
wangboc <447537045@qq.com>
2015-08-19 20:34:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ho/criminalintent/CrimeActivity.java
@@ -0,0 +1,18 @@
+package com.example.ho.criminalintent;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CrimeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
+
+
